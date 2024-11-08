dd0bd4bbfc98439a76a3089ca2cef24dbc129ed4
sanjaypramod <sanjaypramod.p@gmail.com>
2024-11-08 06:04:36 +0000

    added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="multiply_lib",
+    version="0.1.0",
+    author="sanjaypramod",
+    author_email="sanjaypramod.p@gmail.com",
+    description="A Python library for multiplication operations",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if needed
+)
